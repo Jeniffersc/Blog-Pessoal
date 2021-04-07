bc615f2bd4db927a57135e6cf1e99b8ec0c9a493
jeniffersc <jene.santos.campos@gmail.com>
2021-04-07 09:50:03 -0300

    Atualização do Controller e Service do usuário

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,15 +1,18 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin("*")
-public class UsuarioController 
-{	
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioRepository repository;
+
 	@Autowired
 	private UsuarioService usuarioService;
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
@@ -31,7 +42,11 @@ public class UsuarioController
 
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
+		try {
+				return ResponseEntity.ok(user.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
 	}
 }
--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -1,12 +1,14 @@
 package org.generation.blogPessoal.service;
 
-import java.nio.charset.Charset;
-import java.util.Optional;
-import org.apache.commons.codec.binary.Base64;
+
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.repository.UsuarioRepository;
 
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,13 +19,18 @@ public class UsuarioService
 	@Autowired
 	private UsuarioRepository repository;
 
-	public Usuario CadastrarUsuario(Usuario usuario) {
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+		
+		
+		if(repository.findByUsuario(usuario.getUsuario()).isPresent())
+			return null;
+		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 		String senhaEncoder = encoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaEncoder);
 
-		return repository.save(usuario);
+		return Optional.of(repository.save(usuario));
 	}
 
 	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
@@ -41,8 +48,9 @@ public class UsuarioService
 				user.get().setToken(authHeader);				
 				user.get().setNome(usuario.get().getNome());
 				user.get().setSenha(usuario.get().getSenha());
-
+				
 				return user;
+
 			}
 		}
 		return null;
